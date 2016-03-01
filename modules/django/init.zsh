c9b235224eb4173d2f655eb6dc23b87ef3219d66
mohan3d <mohan3d94@gmail.com>
2016-03-01 22:08:21 +0200

    common django aliases

---
+++ modules/django/init.zsh
@@ -0,0 +1,12 @@
+#
+# Aliases
+#
+
+alias dj='python manage.py'
+alias djs='python manage.py shell'
+alias djm='python manage.py migrate'
+alias djmm='python manage.py makemigrations'
+alias djsa='python manage.py startapp'
+alias djcs='python manage.py collectstatic'
+alias djrs='python manage.py runserver'
+alias djcsu='python manage.py createsuperuser'
